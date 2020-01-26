04fd646ac2d85831c8e685a1c8c927f6c2c3e803
iojw <isaacong.jw@gmail.com>
2020-01-26 09:36:20 +0800

    Explicitly set SelectorEventLoop on 3.8, Windows (#13)

--- socialscan/__main__.py
+++ socialscan/__main__.py
@@ -3,11 +3,21 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import asyncio
+import sys
 
 from socialscan import cli
 
 
 def main():
+    # To avoid 'Event loop is closed' RuntimeError due to compatibility issue with aiohttp
+    if sys.platform.startswith("win") and sys.version_info >= (3, 8):
+        try:
+            from asyncio import WindowsSelectorEventLoopPolicy
+        except ImportError:
+            pass
+        else:
+            if not isinstance(asyncio.get_event_loop_policy(), WindowsSelectorEventLoopPolicy):
+                asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
     asyncio.run(cli.main())
 
 
